bb41145fb858286b93fe5832248e03f5c8c8165a
Hadi <rittihadi@gmail.com>
2024-10-05 18:23:43 +0700

    Added neighbor searches
    GetNeighborsToBuffer now works on standard and hexa games

--- src/screen_game.c
+++ src/screen_game.c
@@ -26,6 +26,8 @@ static PR_GameCell* cells;
 static size_t        neighborCount;
 static PR_GameCell** neighborBuffer;
 
+// Gets neighbor cell addresses of the current given cell,
+// gives NULL if none found.
 static void (*GetNeighborsToBuffer)(size_t cellIndex);
 static void (*Draw)(void);
 
@@ -110,6 +112,40 @@ static void StandardGameDraw(void)
 
 static void StandardGameGetNeighbors(size_t cellIndex)
 {
+    int gridWidth  = currentGameParams.classicGame.width;
+    int gridHeight = currentGameParams.classicGame.height;
+    int cellX      = cellIndex % gridWidth;
+    int cellY      = cellIndex / gridWidth;
+
+    size_t offsets[] = {-gridWidth - 1, -gridWidth,
+                        -gridWidth + 1, -1, 1,
+                        gridWidth - 1, gridWidth,
+                        gridWidth + 1};
+
+    // This only bound checks y axis
+    for (size_t i = 0; i < neighborCount; i++)
+    {
+        size_t idx = offsets[i] + cellIndex;
+        if (idx < 0 || idx >= cellCount)
+            neighborBuffer[i] = NULL;
+        else
+            neighborBuffer[i] = &cells[idx];
+    }
+
+    // Handle neighbors wrapped around on the x axis
+    if (cellX == 0)
+    {
+        neighborBuffer[0] = NULL;
+        neighborBuffer[3] = NULL;
+        neighborBuffer[5] = NULL;
+    }
+
+    if (cellX == gridWidth - 1)
+    {
+        neighborBuffer[2] = NULL;
+        neighborBuffer[4] = NULL;
+        neighborBuffer[7] = NULL;
+    }
 }
 
 static void HexaGameDraw(void)
@@ -119,4 +155,92 @@ static void HexaGameDraw(void)
 
 static void HexaGameGetNeighbors(size_t cellIndex)
 {
+    int    gridLength      = currentGameParams.hexaGame.sideLength;
+    size_t cellIndexInLine = cellIndex;
+
+    int    found   = 0;
+    size_t lineIdx = 0;
+    size_t prevLen = 0;
+    size_t currLen = 0;
+    size_t nextLen = 0;
+
+    // Top half
+    for (size_t i = 0; i < gridLength; ++i)
+    {
+        lineIdx++;
+        currLen = gridLength + i;
+        prevLen = currLen - 1;
+        nextLen = currLen + 1;
+        if (cellIndexInLine < currLen)
+        {
+            found = 1;
+            break;
+        }
+        cellIndexInLine -= currLen;
+    }
+
+    // Bottom half
+    for (size_t i = gridLength - 2; i >= 0 && !found; i--)
+    {
+        lineIdx++;
+        currLen = gridLength + i;
+        prevLen = currLen + 1;
+        nextLen = currLen - 1;
+        if (cellIndexInLine < currLen)
+            break;
+        cellIndexInLine -= currLen;
+    }
+
+    // Exactly in the middle
+    if (currLen == (gridLength * 2) - 1)
+        prevLen = nextLen = currLen - 1;
+
+    // Clear neighbor buffer
+    for (size_t i = 0; i < neighborCount; i++)
+        neighborBuffer[i] = NULL;
+
+    size_t maxCurrNext = (1 + nextLen + currLen) >> 1;
+    size_t maxCurrPrev = (1 + prevLen + currLen) >> 1;
+
+    if (cellIndexInLine > 0)
+    {
+        // W neighbor
+        neighborBuffer[2] = &cells[cellIndex - 1];
+        // NW neighbor
+        if (lineIdx > 0)
+            neighborBuffer[0] = &cells[cellIndex - maxCurrPrev];
+        // NE neighbor
+        if (lineIdx < 2 * gridLength - 2)
+            neighborBuffer[1] = &cells[cellIndex - maxCurrPrev + 1];
+    }
+    else
+    {
+        // "Top" half
+        if (currLen < nextLen)
+            neighborBuffer[4] = &cells[cellIndex + maxCurrNext - 1];
+        // "bottom" half
+        if (currLen < prevLen)
+            neighborBuffer[0] = &cells[cellIndex - maxCurrPrev];
+    }
+
+    if (cellIndexInLine < currLen - 1)
+    {
+        // E neighbor
+        neighborBuffer[3] = &cells[cellIndex + 1];
+        // SW neighbor
+        if (lineIdx > 0)
+            neighborBuffer[4] = &cells[cellIndex + maxCurrNext - 1];
+        // SE neighbor
+        if (lineIdx < 2 * gridLength - 2)
+            neighborBuffer[5] = &cells[cellIndex + maxCurrNext];
+    }
+    else
+    {
+        // "Top" half
+        if (currLen < nextLen)
+            neighborBuffer[5] = &cells[cellIndex + maxCurrNext];
+        // "bottom" half
+        if (currLen < prevLen)
+            neighborBuffer[1] = &cells[cellIndex - maxCurrPrev + 1];
+    }
 }
